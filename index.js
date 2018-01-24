704971816ed33ccad945083269c6112dc40e3d91
nikoli-four-six <nikolidaig@gmail.com>
2018-01-24 14:37:51 -0500

    More Consistent Punctuation

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const logger = require('./lib/logger');
 // Get configuration information
 config = JSON.parse(fs.readFileSync(__dirname + "/config.json").toString());
 
-config.ownerID = parseInt(process.argv[2])
-config.token = process.argv[3]
+config.ownerID = parseInt(process.argv[2]);
+config.token = process.argv[3];
 
 // Start the client and input the token
-client = new Akairo.AkairoClient(config);
+client = new Akairo.AkairoClient(config, {disableEveryone: true});
 
 logger.log('core', 'Logging In...');
 
