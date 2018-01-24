8eab4321194962343835e66a612fc5b9780e16ee
nikoli-four-six <nikolidaig@gmail.com>
2018-01-24 14:34:14 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# N46 Bot
+
+A discord bot
+
+## How it Works
+
+To run the bot use `nodejs . <ownerID> <token>` 
---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const Akairo = require('discord-akairo');
+const logger = require('./lib/logger');
+
+// Get configuration information
+config = JSON.parse(fs.readFileSync(__dirname + "/config.json").toString());
+
+config.ownerID = parseInt(process.argv[2])
+config.token = process.argv[3]
+
+// Start the client and input the token
+client = new Akairo.AkairoClient(config);
+
+logger.log('core', 'Logging In...');
+
+client.login(config.token).then(() => {
+  logger.log('core', 'BOT STARTED');
+});
