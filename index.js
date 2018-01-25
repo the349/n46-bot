96b710dfc562459c75b59036c68a7f11a51d9f58
Stian <stian108@users.noreply.github.com>
2018-01-25 20:26:17 +0100

    Semicolon

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
 const Akairo = require('discord-akairo');
 const logger = require('./lib/logger');
-const config = require('./config')
-const defaults = require('./defaults')
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const config = require('./config');
+const defaults = require('./defaults');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
 config.ownerID = parseInt(process.argv[2]);
 config.token = process.argv[3];
 config.emmiters = {
   process: process
-}
+};
 
 class n46Client extends Akairo.AkairoClient {
   constructor(config) {
-    super(config)
+    super(config);
 
     // Set up database
-    const adapter = new FileSync('./lib/db.json')
-    this.db = low(adapter)
-    this.db.defaults(defaults)
+    const adapter = new FileSync('./lib/db.json');
+    this.db = low(adapter);
+    this.db.defaults(defaults);
   }
 }
 
