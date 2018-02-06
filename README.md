baea9a54821a4ea68c81d99d98ac266733b0a414
Stian <shu034@uib.no>
2018-02-06 13:11:34 +0100

    Configs are cooler

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ A discord bot meant for use on a single server
 ## How it Works
 
 1.  To install the bot use `npm install`
-2.  To run the bot use `nodejs . <ownerID> <token>`
+3.  Change the config.json by adding your bot token and owner id  
+2.  To run the bot use `nodejs . <token>` or `nodejs . <token>` if you choose not to save the token
--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const config = require('./config.json');
 const N46Client = require('./lib/bot/client.js');
 const logger = require('./lib/bot/logger');
 
-// Set up db
-config.ownerID = parseInt(process.argv[2]);
-config.token = process.argv[3];
-config.emmiters = {
+if (process.argv[2]) config.client.token = process.argv[2];
+
+config.client.emmiters = {
   process: process
 };
 
@@ -16,7 +15,7 @@ const client = new N46Client(config.client, config, {
 
 logger.log('core', 'Logging In...');
 
-client.login(config.token).then(() => {
+client.login(config.client.token).then(() => {
   logger.log('core', 'BOT STARTED');
   client.updateGuilds();
 });
