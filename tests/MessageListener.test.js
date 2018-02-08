452d7aeb93d5839cedc07577083e8e1c69853e40
nikoli-four-six <nikolidaig@gmail.com>
2018-02-08 00:59:13 -0500

    add cooldown function to client instead of having one per listener

--- tests/MessageListener.test.js
+++ tests/MessageListener.test.js
@@ -5,19 +5,6 @@ const { expect } = require('chai');
 let fake = require('./fake');
 
 describe('MessageListener', function () {
-  describe('createXpWait', function () {
-    it('Create a xp cooldown', function () {
-      MessageListener.createXpWait('u0000', fake.client);
-      expect(fake.client.sessiondb.xpWaits).to.have.property('u0000');
-    });
-
-    this.timeout(6);
-
-    it('Self-destruct xp cooldown', function () {
-      expect(fake.client.sessiondb.xpWaits).not.to.have.property('u0000');
-    });
-  });
-
   describe('checkMetRanks', function () {
     fake.guild.rolegroups = N46Client.updateRoleGroups(fake.guild);
     const fakeRanked = fake.roles.get(4);
