611a7b8853f3fe23b2fd68ab32afab6626253576
nikoli-four-six <nikolidaig@gmail.com>
2018-02-22 12:17:02 -0500

    New bare-bones test bot

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const config = require('./config.json');
-const { AkairoClient } = require('discord-akairo');
+const N46Client = require('./lib/bot/client');
 const EnmapLevel = require('enmap-level');
 const Enmap = require('enmap');
 const level = new EnmapLevel({ name: 'bot' });
 const db = new Enmap({ provider: level });
-
-// Set up db
-db.defer.then(() => {
-  console.log('db', db.size + ' keys loaded');
-});
 
 if (process.argv[2]) config.client.token = process.argv[2];
 
@@ -16,19 +11,15 @@ config.client.emmiters = {
   process: process
 };
 
-class N46Client extends AkairoClient {
-  constructor (config, fullConfig, db) {
-    super(config);
-    this.config = fullConfig;
-    this.db = db;
-  }
-}
-
 // Start the client and input the token
-const client = new N46Client(config.client, config, db);
+const client = new N46Client(config, db);
 
-console.log('core', 'Logging In...');
-
-client.login(config.client.token).then(() => {
-  console.log('core', 'BOT STARTED');
+// Set up db
+db.defer.then(() => {
+  client.logger.info(db.size + ' keys loaded', {module: 'db'});
+  client.logger.info('Logging In...', {module: 'core'});
+  client.login(config.client.token).then(() => {
+    client.logger.info('BOT STARTED', {module: 'core'});
+    client.updateGuilds();
+  });
 });
